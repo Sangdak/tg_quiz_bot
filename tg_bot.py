a7eddac7c5800b64fd215e0b29b4f24c46522759
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-12-20 08:30:25 +1000

    Add async interaction with. redis.

--- tg_bot.py
+++ tg_bot.py
@@ -13,18 +13,6 @@ from get_question import get_random_question
 
 ATTEMPTS = 2  # + 1 попытка
 
-# Словарь, в котором будут храниться данные пользователя
-users = {
-    173901673: {
-        'in_game': True,
-        'current_question': {},
-        'attempts': 0,
-        'total_games': 0,
-        'wins': 0,
-    },
-}
-
-print(users)
 
 load_dotenv('.env')
 
@@ -32,7 +20,6 @@ redis_host = os.environ.get('REDIS_HOST')
 redis_port = os.environ.get('REDIS_PORT')
 redis_db = os.environ.get('REDIS_DATABASES')
 tg_api_token = os.environ.get('TG_TOKEN')
-
 
 bot = Bot(tg_api_token)
 dp = Dispatcher()
@@ -80,25 +67,9 @@ def get_random_question_set() -> dict:
 # Хендлер обработки команды "/start"
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
-    await message.answer(
-        text='Привет!\nДавайте начнём нашу викторину.\n\n'
-        'Чтобы получить правила игры и список доступных '
-        'команд - отправьте команду /help',
-        reply_markup=keyboard_start
-    )
-    # Добавление нового пользователя в словарь users
-    if message.from_user.id not in users:
-        users[message.from_user.id] = {
-            'in_game': False,
-            'current_question': {},
-            'attempts': None,
-            'total_games': 0,
-            'wins': 0
-        }
-
     client = redis.Redis()
     user_exist = await client.get(f'{message.from_user.id}_in_game')
-    print(user_exist)
+
     if user_exist is None:
         await client.set(f'{message.from_user.id}_in_game', 0)
         await client.set(f'{message.from_user.id}_attempts', 0)
@@ -108,6 +79,13 @@ async def process_start_command(message: Message):
         await client.set(f'{message.from_user.id}_current_answer', '')
         await client.set(f'{message.from_user.id}_current_comment', '')
     await client.aclose()
+
+    await message.answer(
+        text='Привет!\nДавайте начнём нашу викторину.\n\n'
+             'Чтобы получить правила игры и список доступных '
+             'команд - отправьте команду /help',
+        reply_markup=keyboard_start
+    )
 
 
 # Хендлер обработки команды "/help"
@@ -129,13 +107,14 @@ async def process_help_command(message: Message):
 async def process_cancel_command(message: Message):
     client = redis.Redis()
     is_user_in_game = await client.get(f'{message.from_user.id}_in_game')
-    if users[message.from_user.id]['in_game']:
-        users[message.from_user.id]['in_game'] = False
+    if bool(is_user_in_game):
+        await client.set(f'{message.from_user.id}_in_game', int(False))
         await message.answer(
             text='Вы вышли из игры. Если захотите '
                  'сыграть снова - нажмите кнопку.',
             reply_markup=keyboard_start,
         )
+
     else:
         await message.answer(
             text='А мы и так с вами не играем. '
@@ -143,36 +122,51 @@ async def process_cancel_command(message: Message):
             reply_markup=keyboard_start,
         )
 
+    await client.aclose()
+
 
 # Хендлер обработки команды "мой счёт"
 @dp.message(F.text.lower().in_(['мой счёт', 'счёт']))
 async def process_stat_command(message: Message):
-    if not users[message.from_user.id]['in_game']:
+    client = redis.Redis()
+    is_user_in_game = await client.get(f'{message.from_user.id}_in_game')
+    total_games_num = await client.get(f'{message.from_user.id}_total_games')
+    wins_num = await client.get(f'{message.from_user.id}_wins')
+    await client.aclose()
+
+    if int(is_user_in_game):
         await message.answer(
             text=f'Всего игр сыграно: '
-                 f'{users[message.from_user.id]["total_games"]}\n'
-                 f'Игр выиграно: {users[message.from_user.id]["wins"]}',
-            reply_markup=keyboard_menu,
+                 f'{int(total_games_num)}\n'
+                 f'Игр выиграно: {int(wins_num)}',
+            reply_markup=keyboard_question,
         )
 
     else:
         await message.answer(
             text=f'Всего игр сыграно: '
-                 f'{users[message.from_user.id]["total_games"]}\n'
-                 f'Игр выиграно: {users[message.from_user.id]["wins"]}',
-            reply_markup=keyboard_question,
+                 f'{int(total_games_num)}\n'
+                 f'Игр выиграно: {int(wins_num)}',
+            reply_markup=keyboard_menu,
         )
+
+
 
 
 # Хендлер обработки выбора юзера сдаться
 @dp.message(F.text.lower().in_(['сдаться', 'пропустить']))
 async def process_user_give_up(message: Message):
-    if users[message.from_user.id]['in_game']:
-        users[message.from_user.id]['in_game'] = False
+    client = redis.Redis()
+    is_user_in_game_cancel = await client.get(f'{message.from_user.id}_in_game')
+    total_games = await client.get(f'{message.from_user.id}_total_games')
+    answer = await client.get(f'{message.from_user.id}_current_answer')
+    if bool(is_user_in_game_cancel):
+        await client.set(f'{message.from_user.id}_in_game', int(False))
+        await client.set(f'{message.from_user.id}_total_games', int(total_games) + 1)
         await message.answer(
             text=f"Ну, раз уж вы решили сдаться, "
             f"вот вам правильный ответ: "
-            f"\n{users[message.from_user.id]['current_question']['answer']}",
+            f"\n{str(answer.decode('utf-8'))}",
             reply_markup=keyboard_answer,
         )
     else:
@@ -181,104 +175,139 @@ async def process_user_give_up(message: Message):
             reply_markup=keyboard_start,
         )
 
+    await client.aclose()
+
 
 # Хендлер обработки команды согласия пользователя сыграть в игру
 @dp.message(F.text.lower().in_(
     ['да', 'давай', 'игра', 'играть', 'начать игру', 'новый вопрос', 'сыграем', 'хочу играть']
 ))
 async def process_positive_answer(message: Message):
-    if not users[message.from_user.id]['in_game']:
-        users[message.from_user.id]['in_game'] = True
-        users[message.from_user.id]['current_question'] = get_random_question_set()
-        users[message.from_user.id]['attempts'] = ATTEMPTS
+    question_set = get_random_question_set()
+    # print('QS', question_set)
+
+    client = redis.Redis()
+    is_user_in_game = await client.get(f'{message.from_user.id}_in_game')
+    if not bool(is_user_in_game):
+        question = await client.get(f'{message.from_user.id}_current_question')
         await message.answer(
-            text=f"Новый вопрос!\n"
-                 f"\n{users[message.from_user.id]['current_question']['question']}",
+            text=f"Вы уже получили вопрос:\n"
+                 f"\n{question.decode('utf-8')}",
             reply_markup=keyboard_question,
         )
+
+    else:
+        await client.set(f'{message.from_user.id}_current_question', question_set['question'])
+        await client.set(f'{message.from_user.id}_current_answer', question_set['answer'])
+        await client.set(f'{message.from_user.id}_current_comment', question_set['comment'])
+        await client.set(f'{message.from_user.id}_attempts', ATTEMPTS)
+        await client.set(f'{message.from_user.id}_in_game', int(True))
+
+        question = await client.get(f'{message.from_user.id}_current_question')
+        await message.answer(
+            text=f"Вопрос:\n"
+                 f"\n{question.decode('utf-8')}",
+            reply_markup=keyboard_question,
+        )
+
+    await client.aclose()
 
 
 # Хендлер обработки запроса комментария
 @dp.message(F.text.lower().in_(['комментарий', 'подробнее', 'подрбности']))
 async def process_user_answer(message: Message):
-
-    if not users[message.from_user.id]['in_game']:
-        if users[message.from_user.id]['current_question']['comment'] != '':
-            await message.answer(
-                text=f"Комментарий к ответу: "
-                     f"{users[message.from_user.id]['current_question']['comment']}",
-                reply_markup=keyboard_menu,
-            )
-        else:
-            await message.answer(
-                text='К этому ответу нет комментариев!',
-                reply_markup=keyboard_menu,
-            )
-
-    else:
+    client = redis.Redis()
+    is_user_in_game = await client.get(f'{message.from_user.id}_in_game')
+    if int(is_user_in_game):
         await message.answer(
             text='Пока не определились с ответом,'
                  'комментарий недоступен!',
             reply_markup=keyboard_question,
         )
 
+    else:
+        comment = await client.get(f'{message.from_user.id}_current_comment')
+        if not comment:
+            await message.answer(
+                text='К этому ответу нет комментариев!',
+                reply_markup=keyboard_menu,
+            )
+
+        else:
+            await message.answer(
+                text=f"Комментарий к ответу: "
+                     f"{comment.decode('utf-8')}",
+                reply_markup=keyboard_menu,
+            )
+
+        await client.aclose()
+
 
 # Хендлер обработки команды отказа пользователя сыграть в игру
 @dp.message(F.text.lower().in_(['завершить игру', 'не', 'нет', 'не буду', 'не хочу']))
 async def process_negative_answer(message: Message):
-    if not users[message.from_user.id]['in_game']:
-        await message.answer(
-            text='Жаль :(\n\nЕсли захотите поиграть - '
-                 'просто напишите об этом',
-            reply_markup=keyboard_start,
-        )
-    else:
+    client = redis.Redis()
+    is_user_in_game = await client.get(f'{message.from_user.id}_in_game')
+    await client.aclose()
+
+    if int(is_user_in_game):
         await message.answer(
             text='Мы же сейчас с вами играем,'
                  'придётся сначала сдаться!',
             reply_markup=keyboard_question,
         )
 
+    else:
+        await message.answer(
+            text='Вы вышли из игры. Если захотите '
+                 'сыграть снова - нажмите кнопку.',
+            reply_markup=keyboard_start,
+        )
+
 
 # Хендлер обработки отправки пользователем ответа
 @dp.message()
-# async def process_numbers_answer(message: Message):
 async def process_user_answer(message: Message):
-    answer_short, answer_addition = users[message.from_user.id]['current_question']['answer'].split('.', maxsplit=1)
+    client = redis.Redis()
+    is_user_in_game = await client.get(f'{message.from_user.id}_in_game')
+    total_games = await client.get(f'{message.from_user.id}_total_games')
+    wins = await client.get(f'{message.from_user.id}_wins')
+    attempts = await client.get(f'{message.from_user.id}_attempts')
+    answer = await client.get(f'{message.from_user.id}_current_answer')
+
+    answer_short, answer_addition = answer.decode('utf-8').split('.', maxsplit=1)
     table = str.maketrans("", "", string.punctuation)
 
-    users[message.from_user.id]['current_question']['answer_short'] = answer_short
-    if answer_addition:
-        users[message.from_user.id]['current_question']['answer_addition'] = answer_addition
-
-    if users[message.from_user.id]['in_game']:
+    if bool(is_user_in_game):
         if message.text.lower() == answer_short.translate(table).lower():
-            users[message.from_user.id]['in_game'] = False
-            users[message.from_user.id]['total_games'] += 1
-            users[message.from_user.id]['wins'] += 1
+            await client.set(f'{message.from_user.id}_in_game', int(False))
+            await client.set(f'{message.from_user.id}_total_games', int(total_games) + 1)
+            await client.set(f'{message.from_user.id}_wins', int(wins) + 1)
+
             await message.answer(
                 text=f"Верно!\n"
-                     f"{users[message.from_user.id]['current_question']['answer_addition'] if 'answer_addition' in users[message.from_user.id]['current_question'] else ''}\n\n"
+                     f"{answer.decode('utf-8')}\n\n"
                      f"Может, сыграем еще?",
                 reply_markup=keyboard_answer,
             )
 
-        elif users[message.from_user.id]['attempts'] == 0:
-            users[message.from_user.id]['in_game'] = False
-            users[message.from_user.id]['total_games'] += 1
+        elif int(attempts) == 0:
+            await client.set(f'{message.from_user.id}_in_game', int(False))
+            await client.set(f'{message.from_user.id}_total_games', int(total_games) + 1)
+
             await message.answer(
                 text=f"К сожалению, у вас больше не осталось "
                      f"попыток. Вы проиграли :(\n\nПравильный ответ: "
-                     f"{users[message.from_user.id]['current_question']['answer']}"
+                     f"{answer.decode('utf-8')}"
                      f"\n\nДавайте сыграем еще?",
                 reply_markup=keyboard_answer,
             )
 
         else:
-            users[message.from_user.id]['attempts'] -= 1
+            await client.set(f'{message.from_user.id}_attempts', int(attempts) - 1)
             await message.answer(
                 text=f"Похоже, это неправильный ответ, "
-                     f"количество оставшихся попыток: {users[message.from_user.id]['attempts'] + 1}, "
+                     f"количество оставшихся попыток: {int(attempts)}, "
                      f"попробуйте ещё раз!",
                 reply_markup=keyboard_question,
             )
@@ -288,6 +317,8 @@ async def process_user_answer(message: Message):
             text='Мы еще не играем. Хотите сыграть?',
             reply_markup=keyboard_start
         )
+
+    await client.aclose()
 
 
 if __name__ == '__main__':
